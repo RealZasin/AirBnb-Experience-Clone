68b38ce7e3f1d173dd38b745018c857142c1eebc
EirBec <eirbec.kodehode@gmail.com>
2024-06-24 12:17:05 +0200

    removed blank space on right side of nav bar/header

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,8 @@ import React from "react"
 
 export default function Navbar() {
     return (
-        <>
-            <img src="airbnb-logo.png" className="nav--logo" />
-        </>
-    )
+      <nav>
+        <img src="airbnb-logo.png" className="nav--logo" />
+      </nav>
+    );
 }
