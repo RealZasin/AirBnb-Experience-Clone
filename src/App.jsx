6ed155671de1b9b065cd63e3566c7ad8704e84a9
EirBec <eirbec.kodehode@gmail.com>
2024-06-24 13:51:32 +0200

    new data for cards added, cards comp updated, and more unique cards added

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,28 @@ import './App.css'
 import Navbar from "./components/Navbar.jsx"
 import Hero from "./components/Hero.jsx"
 import Card from "./components/Card.jsx"
+import data from "./data.jsx"
 
 function App() {
-  
+  const newCard = data.map(cardData => {
+    return (
+    <Card
+    key={cardData.id}
+    img={cardData.coverImg}
+    rating={cardData.stats.rating}
+    reviewCount={cardData.stats.reviewCount}
+    location={cardData.location}
+    title={cardData.title}
+    price={cardData.price}
+    />
+    )
+  })
 
   return (
     <>
       <Navbar />
-      <Hero />
-      <Card />
+      {/* <Hero /> */}
+      {newCard}
     </>
   )
 }
