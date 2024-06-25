1ab4a949837386f246aedff29147d1054260b485
EirBec <eirbec.kodehode@gmail.com>
2024-06-25 13:56:45 +0200

    Thing works, stable version, no errors. Adding pages next now.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,11 @@ import React from "react"
 
 export default function Navbar() {
     return (
-      <nav>
-        <img src="airbnb-logo.png" className="nav--logo" />
-      </nav>
+
+    <nav>
+      <img src="airbnb-logo.png" className="nav--logo" />
+      <p>Next Page</p>
+    </nav>
+    
     );
 }
