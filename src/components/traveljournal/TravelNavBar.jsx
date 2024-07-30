b75a1f7600c2ce9cf640f1fb987b8625f99382c4
EirBec <eirbec.kodehode@gmail.com>
2024-07-30 14:56:04 +0200

    added and tweaked clickable link to take you forward and back a page. aaand that is all for today foks

--- src/components/traveljournal/TravelNavBar.jsx
+++ src/components/traveljournal/TravelNavBar.jsx
@@ -1,11 +1,15 @@
 import styles from "./TravelNavBar.module.css";
 import "../../../public/globe.png";
+import { Link } from "react-router-dom";
 
 export default function NavBar() {
   return (
     <div className={styles.navbar}>
       <img src="globe.png" className={styles.globeimg} />
       <h1>Travel Goals</h1>
+      <span className={styles.BackOnePageFixedLocation}>
+        <Link to="/">Back</Link>
+      </span>
     </div>
   );
 }
