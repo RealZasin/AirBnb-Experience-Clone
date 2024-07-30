b39efda75e1e45562f70aab06aed2819727f21b6
EirBec <eirbec.kodehode@gmail.com>
2024-07-30 14:36:52 +0200

    fixing first page, the airbnb page both mobile and desktop. good stuff

--- src/components/airbnb/AirNavbar.jsx
+++ src/components/airbnb/AirNavbar.jsx
@@ -3,11 +3,13 @@ import {Link } from "react-router-dom";
 
 export default function AirNavbar() {
     return (
-
-    <nav>
-      <img src="airbnb-logo.png" className={styles.navlogo} />
-      <Link to="/traveljournal">Next Page</Link>
-    </nav>
-    
+      <>
+        <nav>
+          <img src="airbnb-logo.png" className={styles.navlogo} />
+        </nav>
+        <span className={styles.NxtPageFixedLocation}>
+          <Link to="/traveljournal">Next Page</Link>
+        </span>
+      </>
     );
 }
